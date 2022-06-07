07bd248cf13d4a431326f7a35380b08ecd667283
Khamal Ismadie <37945835+khamalismadie@users.noreply.github.com>
2022-06-07 21:32:55 +0700

    Tugas 6

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -18,7 +18,32 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = require("fs"); // import fs
+
+bacaData.readFile(file1, "utf8", function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+  JSON.parse(data);
+});
+bacaData.readFile(file2, "utf8", function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+  JSON.parse(data);
+});
+bacaData.readFile(file3, "utf8", function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+  JSON.parse(data);
+});
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
